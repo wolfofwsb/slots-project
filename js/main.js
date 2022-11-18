52eeed5a444ff736f2d501296dea28043e73b89d
wolfofwsb <aidansery@gmail.com>
2022-11-17 20:54:19 -0600

    final product!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-let arrayNumbersHungry = [
 
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-]
+
+const myCryptoArray = [
+    '₿',
+    '🚀 ',
+    '🦊',
+  ]
+  
+  
+  function getRandom() {
+    const random = Math.floor(Math.random() * 3);
+    return myCryptoArray[random];
+  };
+  
+  
+  
+  let slot1 = document.querySelector('#crypto1')
+  let slot2 = document.querySelector('#crypto2')
+  let slot3 = document.querySelector('#crypto3')
+  let resultMessage = document.querySelector('#resultMessage')
+  let button = document.querySelector('button');
+  
+  button.addEventListener('click', play);
+  
+  function play() {
+    slot1.innerText = getRandom()
+    slot2.innerText = getRandom()
+    slot3.innerText = getRandom()
+    if ((slot1.innerText === slot2.innerText) && (slot1.innerText === slot3.innerText)) {
+      resultMessage.innerText = 'You win!'
+    } else {
+      resultMessage.innerText = 'You lose'
+    }
+  }
+  
+  
+  function init() {
+    slot1.innerText = ""
+    slot2.innerText = ""
+    slot3.innerText = ""
+    resultMessage.innerText = ""
+    render();
+  }
